a72212a2523eaa9a35141bdd522d133ee1a28757
mo-pr <preiningmoritz@gmail.com>
2020-01-07 17:35:18 +0100

    V 0.2

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// JavaScript Document
+var images = [
+    './Pics/Pic1.jpg', 
+    './Pics/Pic2', 
+    './Pics/Pic3'
+];
+var num = 0;
+function next() {
+    var slider = document.getElementById('slider');
+    num++;
+    if(num >= images.length) {
+        num = 0;
+    }
+    slider.src = images[num];
+}
+function prev() {
+    var slider = document.getElementById('slider');
+    num--;
+    if(num < 0) {
+        num = images.length-1;
+    }
+    slider.src = images[num];
+}
+function auto(){
+	setInterval(next, 4000);
+}

bcb11fbebf13f84e2e784cd7f64597543224b240
mo-pr <preiningmoritz@gmail.com>
2020-01-24 15:50:50 +0100

    V 0.4.0

--- head.php
+++ head.php
@@ -2,9 +2,12 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<title>Music</title>
 	<link rel="icon" href="logo.png">
+	<link href="/css/bootstrap.min.css" rel="stylesheet">
 	<script src="./js/script.js"></script>
 </head>
 
@@ -35,3 +38,5 @@
 			</div>
 		</nav>
 </header>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="/js/bootstrap.min.js"></script>

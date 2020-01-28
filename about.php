0e2c141a490c0ff4da30023cb1fe6209a92f5310
mo-pr <preiningmoritz@gmail.com>
2020-01-28 19:34:49 +0100

    V 0.4.0

--- about.php
+++ about.php
@@ -1,3 +1,22 @@
 <?php include('head.php');?>
-
+<table>
+	<tbody>
+		<tr>
+			<td>
+				<img src="avatar.png" alt="Avatar" class="avatar">
+			</td>
+			<td>
+				<img src="avatar2.png" alt="Avatar2" class="avatar">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<hr class="line">
+			</td>
+			<td>
+				<hr class="line">
+			</td>
+		</tr>
+	</tbody>
+</table>
 <?php include('footer.php');?>

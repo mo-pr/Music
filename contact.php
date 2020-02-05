87831c4003735822616fa3cbbcdbafce4468c4bb
mo-pr <preiningmoritz@gmail.com>
2020-02-05 13:12:13 +0100

    V 0.4.4

--- contact.php
+++ contact.php
@@ -3,13 +3,13 @@
 <form id="contactform">
 	<div class="input-group mb-3">
   <div class="input-group-prepend">
-<span class="input-group-text" id="basic-addon1">@</span>
+<span class="input-group-text" id="basic-addon1">..</span>
   </div>
   <input type="text" class="form-control" placeholder="Full Name" aria-label="Name" aria-describedby="basic-addon1">
 </div>
 	<div class="input-group mb-3">
   <div class="input-group-prepend">
-<span class="input-group-text" id="basic-addon1">@</span>
+<span class="input-group-text" id="basic-addon1"> @ </span>
   </div>
   <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
 </div>
